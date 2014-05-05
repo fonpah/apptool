7f2fd0e955275e66e7f3987110a1e86d531d479d
fon-pah <fonpah@yahoo.fr>
2014-05-05 21:57:44 +0200

    adding basic auth

---
+++ app_server/_server/basicAuth.js
@@ -0,0 +1,16 @@
+/**
+ * Created by NONGHO on 09.02.14.
+ */
+exports.setBasicAuthConfig = function( app ){
+    var auth = require('http-auth');
+    var basic = auth.basic({
+        realm: "whiteboard app",
+        file: __dirname + "/../data/users.htpasswd" // gevorg:gpass, Sarah:testpass ...
+    });
+    if(appConfig.getRunMode() == 'dev'){
+        app.use(auth.connect(basic));
+    }
+
+
+    return app;
+}
